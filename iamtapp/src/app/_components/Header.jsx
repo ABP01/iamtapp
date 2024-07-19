dbac173508f2aece5fc737f3fa32d7cee37281a4
Armel Bogue <komlanbogue@gmail.com>
2024-07-19 17:12:11 +0000

    "Added Clerk and Radix UI dependencies, updated layout and page components to integrate ClerkProvider"

---
+++ iamtapp/src/app/_components/Header.jsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+
+
+function Header() {
+    
+    return 
+}
+
+export default Header
